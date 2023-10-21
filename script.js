cc525a704b55bbabac694233304cdad56c2fe45c
RemmyKyalo <chaloremmy@gmail.com>
2023-10-21 22:56:39 +0300

    quote-app done

---
+++ script.js
@@ -0,0 +1,22 @@
+const author = document.querySelector(".author");
+const quote = document.querySelector(".quote");
+const new_quote = document.querySelector("#new-quote");
+const tweet = document.querySelector("#tweet");
+const apiUrl = "https://api.themotivate365.com/stoic-quote";
+
+async function getQuote() {
+  const response = await fetch(apiUrl);
+  const data = await response.json();
+
+  quote.textContent = data.quote;
+  author.textContent = data.author;
+  tweet.addEventListener("click", () => shareTweet(data));
+}
+getQuote();
+new_quote.addEventListener("click", () => {
+  getQuote();
+});
+
+const shareTweet = (data) => {
+  window.open(`https://twitter.com/intent/tweet?text=${data.quote}`);
+};
